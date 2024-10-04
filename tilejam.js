56ae93169647e8bc86d405f725726a4fe5af073f
flip909 <flip909@gmail.com>
2024-10-04 12:35:48 +0100

    Create tilejam.js

---
+++ tilejam.js
@@ -0,0 +1,25 @@
+let swiperCard = new Swiper(".tile-container", {
+    grid: {
+      rows: 1,
+    },
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    breakpoints: {
+      639: { slidesPerView: 2 },
+      640: { slidesPerView: 4 },
+      768: { slidesPerView: 5 },
+      1024: { slidesPerView: 6 },
+      1280: { slidesPerView: 8 },
+      1536: { slidesPerView: 8 },
+    },
+  });
